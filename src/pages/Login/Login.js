2d587dfcc2a0fa87a11c1f262e608530e24d5b92
Federico Comelli <FefeJoker.28@gmail.com>
2021-08-18 01:08:46 -0300

    initial commit
    Se creo el theme asi como algunas clases de css.
    Tambien se hizo un sketch del login.

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,25 @@
+import { Button, Input, Text, Heading} from "@chakra-ui/react";
+
+const Login = () => {
+    return(
+        <div>
+            <Heading className={"title-header-position"}>Corralón</Heading>
+            <form>
+                <div>
+                    <Text className={"left-text"}>Usuario</Text>
+                    <Input type="text" placeholder="Ingresar Usuario" variant={"flushed"}/>
+                    <Text className={"left-text"}>Contraseña</Text>
+                    <Input type="password" placeholder="Ingrese Contraseña" variant={"flushed"}/>
+                </div>
+                <div className={"right-text"}>
+                    <Button>
+                        Ingresar
+                    </Button>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Login
